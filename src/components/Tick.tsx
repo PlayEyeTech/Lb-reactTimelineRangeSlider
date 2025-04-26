8635f76f5bff26c563eaa698dfb24ff9fccee292
mixaza <mihajlo@padelytics.ai>
2025-04-26 16:07:00 +0200

    show label on every x minutes

--- src/components/Tick.tsx
+++ src/components/Tick.tsx
@@ -1,33 +1,38 @@
 import React from 'react'
-import { getMinutes } from 'date-fns'
+import {getMinutes} from 'date-fns'
 
-import { TickLabel, TickMarker } from './StyledComponents'
+import {TickLabel, TickMarker} from './StyledComponents'
 
 interface TickProps {
-  tick: {
-    id: string
-    value: number
-    percent: number
-  }
-  count: number
-  format: (value: number) => string
+    tick: {
+        id: string
+        value: number
+        percent: number
+    }
+    count: number
+    format: (value: number) => string,
+    showMinutes: number | undefined
 }
 
-const Tick: React.FC<TickProps> = ({ tick, count, format }) => {
-  const isFullHour = !getMinutes(tick.value)
+const Tick: React.FC<TickProps> = ({tick, count, format, showMinutes}) => {
+    const minutes = getMinutes(tick.value);
 
-  const tickLabelStyle = {
-    marginLeft: `${-(100 / count) / 2}%`,
-    width: `${100 / count}%`,
-    left: `${tick.percent}%`,
-  }
+    const showLabel = (showMinutes ?? 0) % minutes == 0;
+    const tickLabelStyle = {
+        marginLeft: `${-(100 / count) / 2}%`,
+        width: `${100 / count}%`,
+        left: `${tick.percent}%`,
+    }
 
-  return (
-    <>
-      <TickMarker isFullHour={isFullHour} style={{ left: `${tick.percent}%` }} />
-      {isFullHour && <TickLabel style={tickLabelStyle}>{format(tick.value)}</TickLabel>}
-    </>
-  )
+    return (
+        <>
+            <TickMarker
+                isFullHour={showLabel}
+                style={{left: `${tick.percent}%`}}/>
+            {showLabel && <TickLabel
+                style={tickLabelStyle}>{format(tick.value)}</TickLabel>}
+        </>
+    )
 }
 
 export default Tick
